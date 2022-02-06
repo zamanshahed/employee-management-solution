4dbb53ba2e29add3bc7c7fceb900257364781d1b
zamanshahed <shaheduz.zaman@northsouth.edu>
2022-02-07 01:21:32 +0600

    milestone 1: multi-auth done, required: Reg-tweaking

---
+++ app/Http/Middleware/IsOwnerCheck.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsOwnerCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->user()->is_owner==1){
+            // do owner actions AKA go to user-dashboard page!
+            return $next($request);
+        }else{
+            // do employee actions
+            return redirect('home')->with('error','Sorry! You do not have access to this page.');
+        }
+
+        
+    }
+}
